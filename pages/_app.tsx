cbb094acbe3ceac23a007bc8c82071f1b4d77c97
Gonzalo Jose <gonzalojose.dev@gmail.com>
2021-11-18 15:33:34 -0300

    deleting _app.tsx

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider } from "@chakra-ui/provider";
-
-export default function App({Component, pageProps}){
-    return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    )
-}
